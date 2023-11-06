293fcbe40105d8a2feb1baaa57bff2cd8a179e04
Odinaka nnamani <Robertpaschal>
2023-11-06 09:30:03 -0800

    matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,4 @@ def print_matrix_integer(matrix=[[]]):
             if i < len(row) - 1:
                 print("{:d} ".format(num), end="")
             else:
-                print("{:d}".format(num))
+                print("{:d}".format(num))          
