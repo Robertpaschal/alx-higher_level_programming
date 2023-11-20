e2216ea1f2f1056cdffc06b2edcd1494edf3cc22
Odinaka nnamani <Robertpaschal>
2023-11-20 08:15:45 -0800

    error message

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
-    try: 
+    try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        print("Execption: {}".format(err),file=sys.stderr)
+        print("Execption: {}".format(err), file=sys.stderr)
         return False
     else:
         return True
