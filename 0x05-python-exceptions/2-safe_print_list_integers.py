d3f6d16e1630f21475ca10787425a14936679859
Odinaka nnamani <Robertpaschal>
2023-11-20 06:44:30 -0800

    safe print intgers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    
-    for i in range(x):
 
-        try:
-            print("{:d}".format(value), end='')
-            count += 1
-    except (ValueError, TypeError):
+    try:
+        for i in range(x):
+            value = my_list[i]
+            if isinstance(value, int):
+                print("{:d}".format(value), end='')
+                count += 1
+    except (IndexError, ValueError, TypeError):
         pass
     finally:
         print()
