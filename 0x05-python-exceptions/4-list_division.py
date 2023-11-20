84448b0ca110f4036df9a14f8c0f55505c952fa2
Odinaka nnamani <Robertpaschal>
2023-11-20 11:15:04 -0800

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     n_list = []
-
     for i in range(list_length):
         try:
             div = my_list_1[i] / my_list_2[i]
@@ -9,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
             div = 0
         except ZeroDivisionError:
-            print("divison by 0")
+            print("division by 0")
             div = 0
         except IndexError:
             print("out of range")
