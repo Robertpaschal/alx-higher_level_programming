66f2454f91d08d3efbde6639cb76fe10f898b3c3
Odinaka nnamani <Robertpaschal>
2023-11-20 11:11:44 -0800

    print intger err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         integer_value = int(value)
-        print("{:d}".format(intger_value))
+        print("{:d}".format(integer_value))
         return True
     except ValueError as e:
         print("Exception:", e, file=sys.stderr)
