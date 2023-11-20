5e433d544dc911da870487ef71e616605c0130a3
Odinaka nnamani <Robertpaschal>
2023-11-20 10:54:13 -0800

    print integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(value, int):
                 print("{:d}".format(value), end='')
                 count += 1
-    except (IndexError, ValueEroor, TypeError):
+    except (IndexError, ValueError, TypeError):
         pass
     finally:
         print()
