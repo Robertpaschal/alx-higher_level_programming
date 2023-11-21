6de2ed6d2a29e032cb39a311ca3e2ee6c2c28ff3
Odinaka nnamani <Robertpaschal>
2023-11-21 07:45:01 -0800

    square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 class Square:
-    """ This function has no attributes """
+    """ This class represents a square.
+
+    Attributes:
+    No attributes defined in this empty class.
+
+    Methods:
+    No methods defined in this empty class
+
+    """
     pass
