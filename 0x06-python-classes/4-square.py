83e9bff12c486d066d633a8b8eafa9c178f9e088
Odinaka nnamani <Robertpaschal>
2023-11-21 15:54:30 -0800

    private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -29,40 +29,41 @@ class Square:
 
     @property
     def size(self):
-           """
-           Retrives the size of the square.
+        """
+        Retrives the size of the square.
 
-           Returns:
-           int: The size of the square
-           """
+        Returns:
+        int: The size of the square
+        """
 
-           return self.__size
+        return self.__size
+
     @size.setter
     def size(self, value):
-           """
-           Sets the size of the square
+        """
+        Sets the size of the square
 
-           Parameters:
-           value (int): The size to set.
+        Parameters:
+        value (int): The size to set.
 
-           Raises:
-           TypeError: If size is not an integer.
-           ValueError: If size is less than 0.
-           """
+        Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+        """
 
-           if not isinstance(value, int):
-               raise TypeError("size must be an integer")
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
 
-           if value < 0:
-               raise ValueError("size must be >= 0")
+        if value < 0:
+            raise ValueError("size must be >= 0")
 
-           self.__size = value
+        self.__size = value
 
     def area(self):
-            """
-            Calculates and returns the area of the swuare.
+        """
+        Calculates and returns the area of the swuare.
 
-            Returns:
-            int: The area of the square.
-            """
-            return self.__size ** 2
+        Returns:
+        int: The area of the square.
+        """
+        return self.__size ** 2
