5deb2c967b5e33dd24b59ddd20553c6641593dbc
Odinaka <nnamani.odinakarobert@gmail.com>
2024-01-17 05:42:17 -0800

    not a comedy

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,4 +1,3 @@
-
 -- lists all shows without the genre Comedy in the database hbtn_0d_tvshows
 -- uses a database to list all rows not linked to one row
 SELECT title
@@ -10,4 +9,4 @@ LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy')
 GROUP BY title
-ORDER BY titile ASC;
+ORDER BY title ASC;
