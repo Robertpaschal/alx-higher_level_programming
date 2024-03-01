80637607c8f1a7f083872db39799fc8fa0f8b5f2
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-01 18:17:00 +0100

    status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script fetches https://alx-intranet.hbtn.qo/status
+and displays the body of the response
+"""
+
+import requests
+
+def main():
+    """
+    Fetches the URL https://alx-intranet.hbtn.io/status
+    and displays the body of the response
+    """
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()
+    body = response.text
+
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+except requests.exceptions.RequestException as e:
+    print("Error fetching the URL:", e)
+
+
+if __name__ == "__main__":
+    main()
