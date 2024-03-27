5feef24af472a8b78dd715daddced411fd83905a
Odinaka Nnamani <nnamani.odinakarobert@gmail.com>
2024-03-27 07:22:12 +0100

    js readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,23 +1,10 @@
 #!/usr/bin/node
-
-'use strict';
-
-const fs = require('fs');
-
-function readFile (filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
     if (err) {
-      console.error('An error occured:', err);
+      console.log(err);
       return;
     }
     console.log(data);
   });
-}
-
-if (process.argv.length !== 2) {
-  console.error('Usage: ./readme.js <file_path>');
-  process.exit(1);
-}
-
-const filePath = process.argv[1];
-readFile(filePath);
